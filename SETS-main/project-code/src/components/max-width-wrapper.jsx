aa63fe0beb11e73c23448e631d05a61bac3fdbd6
Hanumath <hvp6533@mavs.uta.edu>
2025-11-10 17:51:15 -0600

    Hospital management system updated

--- SETS-main/project-code/src/components/max-width-wrapper.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function MaxWidthWrapper({ children }) {
-  return (
-    <div className="mx-auto w-full max-w-screen-lg md:px-2.5">{children}</div>
-  );
-}
